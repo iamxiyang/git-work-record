e6d53770e20a24bcbd4e47be113d494d5509b2c7
何喜阳 <iamxiyang@foxmail.com>
2022-10-30 22:43:51 +0800

    chore

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from "/Users/hexiyang/Code/git-work-record/src/index";
-export { default } from "/Users/hexiyang/Code/git-work-record/src/index";
--- dist/index.mjs
+++
@@ -1,6 +0,0 @@
-import jiti from "file:///Users/hexiyang/Code/git-work-record/node_modules/.pnpm/jiti@1.16.0/node_modules/jiti/lib/index.js";
-
-/** @type {import("/Users/hexiyang/Code/git-work-record/src/index")} */
-const _module = jiti(null, { interopDefault: true, esmResolve: true })("/Users/hexiyang/Code/git-work-record/src/index.ts");
-
-export default _module;
